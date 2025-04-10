e7b3ddd5ba1ae7fed2eeaa0d98cb14ff719e428f
olmurphy <owen261@icloud.com>
2025-04-10 19:33:44 -0400

    added more queries

--- src/services/playbook/playbookService.ts
+++ src/services/playbook/playbookService.ts
@@ -3,6 +3,7 @@ import { batchApiClient } from "../core/batchApi";
 import { useCallback } from "react";
 
 export type Playbook = {
+  id: string;
   title: string;
   description: string;
   owners: string;
@@ -29,16 +30,57 @@ export const usePlaybook = () => {
     },
   })
 
+  const updateUserMutation = useMutation({
+    mutationFn: (playbook: Playbook) =>
+      batchApiClient.put<Playbook>(`${PLAYBOOK_API_ENDPOINT}/${playbook.id}`, playbook) as Promise<Playbook>,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['users'] });
+    },
+  });
+
+  const deleteUserMutation = useMutation({
+    mutationFn: (playbookId: number) =>
+      batchApiClient.delete<void>(`${PLAYBOOK_API_ENDPOINT}/${playbookId}`) as Promise<void>,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['users'] });
+    },
+  });
+
   const createPlaybook = useCallback((playbook: Playbook) => {
     createPlaybookMutation.mutate(playbook);
   }, [createPlaybookMutation]);
+
+  const updatePlaybook = useCallback(
+    (playbook: Playbook) => {
+      updateUserMutation.mutate(playbook);
+    },
+    [updateUserMutation]
+  );
+
+  const deletePlaybook = useCallback(
+    (playbookId: number) => {
+      deleteUserMutation.mutate(playbookId);
+    },
+    [deleteUserMutation]
+  );
 
   return {
     playbooks,
     isLoading,
     isError,
     error,
-    refetch,
     createPlaybook,
+    updatePlaybook,
+    deletePlaybook,
+    refetch,
+    isCreateLoading: createPlaybookMutation.isLoading,
+    isUpdateLoading: updateUserMutation.isLoading,
+    isDeleteLoading: deleteUserMutation.isLoading,
+    isCreateError: createPlaybookMutation.isError,
+    isUpdateError: updateUserMutation.isError,
+    isDeleteError: deleteUserMutation.isError,
+    createError: createPlaybookMutation.error,
+    updateError: updateUserMutation.error,
+    deleteError: deleteUserMutation.error
   }
 }
